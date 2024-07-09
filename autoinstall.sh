f6b72a1831ec41f422d025778c9b0d94e286d13a
Ales Komarek <ales-komarek@seznam.cz>
2024-07-09 15:24:02 +0200

    basic disk partitioner

---
+++ autoinstall.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+# Set the keyboard layout to US
+loadkeys us
+
+# test the internet connection
+
+if [[ $(ping -c 1 archlinux.org) ]]; then
+  echo "Internet connection is working fine. Proceeding with the installation"
+else
+  echo "Internet connection is not working. Please check your connection"
+  exit 1
+fi
+
+# Update the system clock
+timedatectl set-ntp true
+
+# Partition the disk
+ 
+ if [[ $(./parts/disk-partitioner.sh)]]; then
+   echo "Disk partitioning completed successfully"
+ else
+   echo "Disk partitioning failed. Exiting..."
+   exit 1
+ fi
---
+++ parts/disk-partitioner.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -e
+
+# prompt the user to confirm the partitioning
+
+read -r -p "This script will partition your disk. Are you sure? (y/N) " response
+case "$response" in
+    [yY])
+        # Proceed with partitioning
+        ;;
+    *)
+        echo "Aborting..."
+        exit 1
+        ;;
+esac
+
+# List the available disks
+
+lsblk
+
+# Prompt the user to select a disk
+
+read -r -p "Enter the disk you want to partition (e.g. /dev/sda): " disk
+
+# Check if the disk exists
+
+if [ ! -b "$disk" ]; then
+    echo "Error: Disk not found"
+    exit 1
+fi
+
+# Wipe the disk
+
+echo "Wiping the disk..."
+sgdisk --zap-all "$disk"
+
+# Create the partitions
+
+echo "Creating partitions..."
+
+sfdisk "$disk" << EOF
+label: gpt
+gpt_ parted
+
+# 1GB EFI partition (adjust size if needed)
+name=1: type=ef00, size=1G
+
+# Remaining space for Linux partition
+name=2: type=8300, start=0, size=\$  # Use entire remaining space
+EOF
+
+# Format the partitions
+
+echo "Formatting partitions..."
+
+mkfs.fat -F32 "${disk}1"
+mkfs.btrfs "${disk}2"
+
+# make the btrfs subvolumes
+
+echo "Creating btrfs subvolumes..."
+
+mount "${disk}2" /mnt
+
+cd /mnt
+
+btrfs subvolume create _active
+btrfs subvolume create _active/rootvol
+btrfs subvolume create _active/homevol
+btrfs subvolume create _snapshots
+btrfs subvolume create _swap
+
+cd /
+
+umount /mnt
+
+# Mount the partitions
+
+echo "Mounting partitions..."
+
+mount -o noatime,compress=lzo,space_cache,subvol=_active/rootvol "${disk}2" /mnt
+mkdir -p /mnt/boot/efi
+mkdir /mnt/home
+mkdir  -p /mnt/mnt/defvol
+
+mount "${disk}1" /mnt/boot/efi
+mount -o noatime,compress=lzo,space_cache,subvol=_active/homevol "${disk}2" /mnt/home
+mount -o noatime,compress=lzo,space_cache,subvol=/  "${disk}2" /mnt/mnt/defvol
